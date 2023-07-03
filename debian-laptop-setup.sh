c6bf531d848ff2cc0b4325804574e2b903fe0e57
4729275 <83101013+4729275@users.noreply.github.com>
2023-07-03 19:09:34 -0400

    Update debian-laptop-setup.sh

--- debian-laptop-setup.sh
+++ debian-laptop-setup.sh
@@ -1,61 +1,46 @@
 #! /bin/bash
 
-### Fedora Laptop Setup Script ###
+### Debian Laptop Setup Script ###
 # Kenneth Simmons, 2023
 
-echo "Fedora Laptop Setup - Kenneth Simmons, 2023"
+echo "Debian Laptop Setup - Kenneth Simmons, 2023"
 
-# Configure DNF
-echo "Configuring DNF:"
-echo "fastestmirror=True" | tee -a /etc/dnf/dnf.conf
-echo "max_parallel_downloads=5" | tee -a /etc/dnf/dnf.conf
-echo "defaultyes=True" | tee -a /etc/dnf/dnf.conf
-echo "keepcache=True" | tee -a /etc/dnf/dnf.conf
+# Configure APT
+echo "Configuring APT:"
+sed -i '1,2d' /etc/apt/sources.list
+sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
 
 # Update the system
 echo "Updating the system:"
-dnf update -y
-
-# Enable RPM Fusion
-echo "Enabling RPM Fusion:"
-dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf groupupdate -y core
-
-# Set hostname
-echo "Setting hostname:"
-read hostname
-hostnamectl set-hostname $hostname
-
-# Enable multimedia codecs
-echo "Enabling multimedia codecs:"
-dnf swap -y ffmpeg-free ffmpeg --allowerasing
-dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-dnf groupupdate -y sound-and-video
-dnf install -y intel-media-driver
+apt update -y && apt full-upgrade -y && apt autoremove -y
 
 # Install packages
 echo "Installing packages:"
-dnf install -y audacity exa file-roller gimp gnome-extensions-app gnome-tweaks google-roboto-fonts htop kleopatra neofetch nextcloud-client thunderbird tilem timeshift vim-enhanced vlc wireguard-tools xsensors
+apt install -y audacity dbus-x11 exa flatpak fonts-roboto gimp git gnome-software-plugin-flatpak htop neofetch nextcloud-desktop psensor thunderbird tilem timeshift ttf-mscorefonts-installer vim vlc wireguard-tools
+apt remove -y firefox-esr libreoffice*
+apt autoremove -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub com.github.GradienceTeam.Gradience
+flatpak install -y flathub org.kde.kleopatra
+flatpak install -y flathub org.libreoffice.LibreOffice
 flatpak install -y flathub org.onlyoffice.desktopeditors
 flatpak install -y flathub us.zoom.Zoom
-
-# Configure bash prompt
-echo "Configuring bash prompt:"
-echo "PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" | tee -a /home/kenneth/.bashrc
+cd /usr/share/themes
+wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.8/adw-gtk3v4-8.tar.xz
+tar -xf adw-gtk3v4-8.tar.xz
+rm adw-gtk3v4-8.tar.xz
+cd
 
 # Create bash aliases
 echo "Creating bash aliases:"
-echo "if [ -f ~/.bash_aliases ]; then" | tee -a /home/kenneth/.bashrc
-echo ". ~/.bash_aliases" | tee -a /home/kenneth/.bashrc
-echo "fi" | tee -a /home/kenneth/.bashrc
 touch /home/kenneth/.bash_aliases
 chown kenneth:kenneth /home/kenneth/.bash_aliases
 echo "alias ls='exa -al --group-directories-first --color=always'" | tee -a /home/kenneth/.bash_aliases
 echo "alias wgup='sudo wg-quick up kenneth-home'" | tee -a /home/kenneth/.bash_aliases
 echo "alias wgdn='sudo wg-quick down kenneth-home'" | tee -a /home/kenneth/.bash_aliases
 echo "alias khome='ssh -i \$HOME/.ssh/kenneth-home -p 314 kenneth@192.168.1.20'" | tee -a /home/kenneth/.bash_aliases
-echo "alias dnfup='sudo dnf update && flatpak update'" | tee -a /home/kenneth/.bash_aliases
+echo "alias aptup='sudo apt update && sudo apt upgrade && sudo apt autoremove && flatpak update'" | tee -a /home/kenneth/.bash_aliases
 
 # Fix display issues
 echo "Fixing display issues:"
@@ -66,7 +51,7 @@ touch /etc/modprobe.d/i915.conf
 echo "options i915 enable_psr=0" | tee -a /etc/modprobe.d/i915.conf
 echo "options i915 enable_guc=3" | tee -a /etc/modprobe.d/i915.conf
 echo "options i915 enable_fbc=1" | tee -a /etc/modprobe.d/i915.conf
-dracut --force
+update-initramfs -u
 
 # Enable fractional scaling
 echo "Enabling fractional scaling:"
