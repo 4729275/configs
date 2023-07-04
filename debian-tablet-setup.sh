d9cb81e2fdcbcc1079788c9bb768b38ce4fefd22
4729275 <83101013+4729275@users.noreply.github.com>
2023-07-03 22:04:28 -0400

    Update debian-tablet-setup.sh

--- debian-tablet-setup.sh
+++ debian-tablet-setup.sh
@@ -25,7 +25,7 @@ update-grub
 
 # Install packages
 echo "Installing packages:"
-apt install -y dbus-x11 exa flatpak fonts-roboto git gnome-software-plugin-flatpak htop neofetch nextcloud-desktop psensor thunderbird ttf-mscorefonts-installer vim wireguard-tools xournalpp
+apt install -y dbus-x11 exa flatpak fonts-roboto git gnome-software-plugin-flatpak htop neofetch nextcloud-desktop psensor systemd-zram-generator thunderbird ttf-mscorefonts-installer vim wireguard-tools xournalpp
 apt remove -y firefox-esr libreoffice*
 apt autoremove -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -40,6 +40,11 @@ wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.8/adw-gtk3v4-
 tar -xf adw-gtk3v4-8.tar.xz
 rm adw-gtk3v4-8.tar.xz
 cd
+
+# Setup swap-on-zram
+echo "Setting up swap-on-zram:"
+echo "zram-size = min(ram, 8192)" | tee -a /etc/systemd/zram-generator.conf
+echo "compression-algorithm = zstd" | tee -a /etc/systemd/zram-generator.conf
 
 # Configure GRUB
 echo "Configuring GRUB:"
