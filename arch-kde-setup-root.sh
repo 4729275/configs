71883c6070bf305906955359f80d053b49baf2c2
4729275 <83101013+4729275@users.noreply.github.com>
2023-07-21 21:50:00 -0400

    Update arch-kde-setup-root.sh

--- arch-kde-setup-root.sh
+++ arch-kde-setup-root.sh
@@ -16,7 +16,8 @@ pacman -Sy
 
 # Install packages
 echo "Installing packages:"
-pacman -S --noconfirm audacity audio-convert exa firefox gimp handbrake hplip htop inkscape kid3 libdvdcss libreoffice-fresh libxcrypt-compat man-db musescore neofetch nextcloud-client ntfs-3g obs-studio psensor qemu-base qt6-wayland steam thunderbird ufw virt-manager wireguard-tools
+pacman -S --noconfirm audacity audio-convert exa firefox gimp handbrake hplip htop inkscape kid3 libdvdcss libreoffice-fresh libxcrypt-compat man-db musescore neofetch nextcloud-client ntfs-3g obs-studio psensor qemu-base qt6-wayland steam thunderbird ufw virt-manager virtualbox wireguard-tools
+usermod -aG vboxusers kenneth
 cd /home/kenneth/Downloads
 wget https://sourceforge.net/projects/fabiololix-os-archive/files/Packages/qt5-webkit-5.212.0alpha4-19-x86_64.pkg.tar.zst ### UPDATE LINK IF IT BECOMES OBSOLETE ###
 cd -
