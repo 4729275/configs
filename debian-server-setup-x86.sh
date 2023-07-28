18794d71df2c93f050a8ecb08f7e12300aef6126
4729275 <83101013+4729275@users.noreply.github.com>
2023-07-28 11:59:20 -0400

    Update debian-server-setup-x86.sh

--- debian-server-setup-x86.sh
+++ debian-server-setup-x86.sh
@@ -24,7 +24,15 @@ usermod -s /bin/bash kenneth
 
 # Install packages
 echo "Installing packages:"
-apt install -y exa htop neofetch snap sudo ufw vim wget
+apt install -y ca-certificates curl exa gnupg htop neofetch snap sudo ufw vim wget
+
+# Install docker
+echo "Installing docker:"
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Configure unattended-upgrades
 echo "Configuring unattended-upgrades:"
