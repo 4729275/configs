8b517a09c5d6268bbf5bec459e394d51159b80fa
4729275 <83101013+4729275@users.noreply.github.com>
2023-07-28 11:55:48 -0400

    Update and rename debian-server-setup-x86 to debian-server-setup-x86.sh

---
+++ debian-server-setup-x86.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+### Debian Server Setup Script ###
+# Kenneth Simmons, 2023
+
+echo "Debian Server Setup - x86 - Kenneth Simmons, 2023"
+
+# Update the system
+echo "Updating the system:"
+apt update
+apt dist-upgrade -y
+apt autoremove -y
+
+# Set hostname
+echo "Setting hostname:"
+read hostname
+hostnamectl set-hostname $hostname
+
+# Create user account
+echo "Creating user account:"
+useradd -m -g users -G sudo kenneth
+passwd kenneth
+usermod -s /bin/bash kenneth
+
+# Install packages
+echo "Installing packages:"
+apt install -y exa htop neofetch snap sudo ufw vim wget
+
+# Configure unattended-upgrades
+echo "Configuring unattended-upgrades:"
+apt install -y unattended-upgrades
+vim /etc/apt/apt.conf.d/50unattended-upgrades
+systemctl enable --now unattended-upgrades
+
+# Set sudo privileges
+echo "Setting sudo privileges:"
+EDITOR=vim visudo
+
+# Setup monthly reboots
+echo "Setting up monthly reboots:"
+echo "0 0 1 * * root /sbin/reboot" | tee -a /etc/crontab
+
+# Create bash aliases
+echo "Creating bash aliases:"
+touch /home/kenneth/.bash_aliases
+echo "alias ls='exa -al --group-directories-first --color=always'" | tee -a /home/kenneth/.bash_aliases
+echo "alias aptup='sudo apt update && sudo apt upgrade && sudo apt autoremove'" | tee -a /home/kenneth/.bash_aliases
+chown kenneth:users /home/kenneth/.bash_aliases
+
+echo "Complete!"
+echo "Reboot the server to finalize changes."
+echo "Remember to set up firewall and SSH keys!"
