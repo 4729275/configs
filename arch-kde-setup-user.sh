79dba68fae1eed8fe9a9498d3e1b0698406c927c
4729275 <83101013+4729275@users.noreply.github.com>
2023-07-30 12:59:18 -0400

    Update arch-kde-setup-user.sh

--- arch-kde-setup-user.sh
+++ arch-kde-setup-user.sh
@@ -15,14 +15,13 @@ cd ..
 
 # Install AUR packages
 echo "Installing AUR packages - manual confirmation required:"
-paru -S amdgpu-pro-oglp davinci-resolve google-earth-pro gtkpod libtxc_dxtn opencl-amd teamviewer tilem tilp timeshift-bin ttf-ms-fonts ttf-roboto-fontconfig ventoy-bin
+paru -S amdgpu-pro-oglp davinci-resolve google-earth-pro gtkpod libtxc_dxtn opencl-amd teamviewer tilem tilp timeshift-bin ttf-ms-fonts ttf-roboto-fontconfig ventoy-bin zoom
 paru -R opencl-mesa
 sudo systemctl enable --now teamviewerd
 
 # Install Flatpak packages
 echo "Installing Flatpak packages:"
 flatpak install -y flathub org.onlyoffice.desktopeditors
-flatpak install -y flathub us.zoom.Zoom
 
 echo "User portion complete!"
 echo "Reboot the system to finalize the changes."
