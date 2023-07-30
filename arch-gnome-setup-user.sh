facbfb36f86d412b90e2f3a58ae4979da5459ccd
4729275 <83101013+4729275@users.noreply.github.com>
2023-07-30 12:59:38 -0400

    Update arch-gnome-setup-user.sh

--- arch-gnome-setup-user.sh
+++ arch-gnome-setup-user.sh
@@ -15,7 +15,7 @@ cd ..
 
 # Install AUR packages
 echo "Installing AUR packages - manual confirmation required:"
-paru -S adw-gtk3 amdgpu-pro-oglp davinci-resolve google-earth-pro gtkpod libtxc_dxtn opencl-amd teamviewer tilem tilp timeshift-bin ttf-ms-fonts ttf-roboto-fontconfig ventoy-bin
+paru -S adw-gtk3 amdgpu-pro-oglp davinci-resolve google-earth-pro gtkpod libtxc_dxtn opencl-amd teamviewer tilem tilp timeshift-bin ttf-ms-fonts ttf-roboto-fontconfig ventoy-bin zoom
 paru -R opencl-mesa
 sudo systemctl enable --now teamviewerd
 
@@ -23,7 +23,6 @@ sudo systemctl enable --now teamviewerd
 echo "Installing Flatpak packages:"
 flatpak install -y flathub com.github.GradienceTeam.Gradience
 flatpak install -y flathub org.onlyoffice.desktopeditors
-flatpak install -y flathub us.zoom.Zoom
 
 echo "User portion complete!"
 echo "Reboot the system to finalize the changes."
