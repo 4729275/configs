aa8065726b8133a849dc12e6ecdef7bafef48a5e
4729275 <83101013+4729275@users.noreply.github.com>
2023-09-26 11:39:24 -0400

    Update arch-gnome-install.sh

--- arch-gnome-install.sh
+++ arch-gnome-install.sh
@@ -7,9 +7,9 @@ echo "Arch Linux GNOME Installer - Kenneth Simmons, 2023"
 
 # Install packages
 echo "Installing GNOME packages:"
-pacman -S eog evince gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-clocks gnome-console gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-text-editor gnome-tweaks gnome-user-share gnome-weather grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb nautilus sushi totem tracker3-miners xdg-desktop-portal xdg-desktop-portal-gnome xdg-user-dirs-gtk
+pacman -S --noconfirm eog evince gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-clocks gnome-console gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-text-editor gnome-themes-extra gnome-tweaks gnome-user-share gnome-weather grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb nautilus pipewire-jack sushi totem tracker3-miners wireplumber xdg-desktop-portal xdg-desktop-portal-gnome xdg-user-dirs-gtk
 echo "Installing other packages:"
-pacman -S firefox thunderbird
+pacman -S --noconfirm firefox rhythmbox thunderbird ttf-liberation
 
 # Enable gdm:
 echo "Enabling gdm:"
