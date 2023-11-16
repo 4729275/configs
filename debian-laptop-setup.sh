c438a8c075b437fddc12cbb4df2b3f82d271fe79
4729275 <83101013+4729275@users.noreply.github.com>
2023-11-16 13:56:54 -0500

    Update debian-laptop-setup.sh

--- debian-laptop-setup.sh
+++ debian-laptop-setup.sh
@@ -17,7 +17,7 @@ apt-get autoremove -y
 
 # Install packages
 echo "Installing packages:"
-apt-get install -y audacity curl dbus-x11 exa flatpak fonts-roboto gimp gnome-console gnome-software-plugin-flatpak htop kleopatra neofetch nextcloud-desktop psensor scdaemon systemd-zram-generator tilem timeshift ttf-mscorefonts-installer ufw vim virt-manager vlc wireguard-tools
+apt-get install -y audacity curl dbus-x11 exa flatpak fonts-roboto gimp gnome-console gnome-software-plugin-flatpak htop kleopatra neofetch nextcloud-desktop psensor scdaemon systemd-timesyncd systemd-zram-generator tilem timeshift ttf-mscorefonts-installer ufw vim virt-manager vlc wireguard-tools
 apt-get remove -y firefox-esr gnome-terminal libreoffice*
 apt-get autoremove -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
