afee5d849a0d74d45517011b550d378be40ae383
4729275 <83101013+4729275@users.noreply.github.com>
2024-05-12 21:33:21 -0400

    Update arch-setup-user.sh

--- arch-setup-user.sh
+++ arch-setup-user.sh
@@ -22,7 +22,7 @@ sudo systemctl enable --now teamviewerd
 
 # Install Flatpak packages
 echo "Installing Flatpak packages:"
-flatpak install -y flathub org.gabmus.gfeeds com.github.tchx84.Flatseal org.gnome.World.PikaBackup org.onlyoffice.desktopeditors org.raspberrypi.rpi-imager us.zoom.Zoom
+flatpak install -y flathub com.github.tchx84.Flatseal io.gitlab.news_flash.NewsFlash org.gnome.World.PikaBackup org.onlyoffice.desktopeditors org.raspberrypi.rpi-imager us.zoom.Zoom
 
 echo "User portion complete!"
 echo "Reboot the system to finalize the changes."
