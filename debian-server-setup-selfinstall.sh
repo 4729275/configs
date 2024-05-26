a39480840e274189dd8a38e70cb51e4429596870
4729275 <83101013+4729275@users.noreply.github.com>
2024-05-26 19:51:05 -0400

    Update debian-server-setup-selfinstall.sh

--- debian-server-setup-selfinstall.sh
+++ debian-server-setup-selfinstall.sh
@@ -8,7 +8,7 @@ echo "Debian Server Setup - Self-Installed System - Kenneth Simmons, 2024"
 # Install packages
 echo "Installing packages:"
 apt-get update
-apt-get install -y ca-certificates curl exa gnupg htop neofetch snapd systemd-timesyncd vim wget
+apt-get install -y ca-certificates curl exa gnupg htop snapd systemd-timesyncd vim wget
 
 # Install docker
 echo "Installing docker:"
