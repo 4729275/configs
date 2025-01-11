1cfb15cad77de2603d15496e3197d99a56e3b048
4729275 <83101013+4729275@users.noreply.github.com>
2025-01-11 15:43:20 -0500

    Update debian-server-setup-selfinstall.sh

--- debian-server-setup-selfinstall.sh
+++ debian-server-setup-selfinstall.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 ### Debian Server Setup Script for a Self-Installed System ###
-# Kenneth Simmons, 2024
+# Kenneth Simmons, 2025
 
-echo "Debian Server Setup - Self-Installed System - Kenneth Simmons, 2024"
+echo "Debian Server Setup - Self-Installed System - Kenneth Simmons, 2025"
 
 # Install packages
 echo "Installing packages:"
@@ -13,9 +13,9 @@ apt-get install -y ca-certificates curl exa gnupg htop snapd systemd-timesyncd v
 # Install docker
 echo "Installing docker:"
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 usermod -aG docker kenneth
