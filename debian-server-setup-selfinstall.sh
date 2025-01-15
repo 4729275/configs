04cc76e8174582a4519f27fc988fdef3c849cda9
4729275 <83101013+4729275@users.noreply.github.com>
2025-01-15 12:48:29 -0500

    Update debian-server-setup-selfinstall.sh

--- debian-server-setup-selfinstall.sh
+++ debian-server-setup-selfinstall.sh
@@ -13,7 +13,7 @@ apt-get install -y ca-certificates curl exa gnupg htop snapd systemd-timesyncd v
 # Install docker
 echo "Installing docker:"
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.asc
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
