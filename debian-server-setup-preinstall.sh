0e2408c3dd123d0a9e21076148db3da536bdfc63
4729275 <83101013+4729275@users.noreply.github.com>
2025-01-15 12:48:47 -0500

    Update debian-server-setup-preinstall.sh

--- debian-server-setup-preinstall.sh
+++ debian-server-setup-preinstall.sh
@@ -45,7 +45,7 @@ apt-get install -y ca-certificates curl exa gnupg htop snapd sudo systemd-timesy
 # Install docker
 echo "Installing docker:"
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.asc
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
