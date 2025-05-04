2104798a1a8ee4e8df62eb40d0ffb3139121047a
4729275 <83101013+4729275@users.noreply.github.com>
2025-05-03 22:37:59 -0400

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -25,9 +25,7 @@ dnf swap ffmpeg-free ffmpeg --allowerasing -y
 dnf --setopt=install_weak_deps=False update @multimedia --exclude=PackageKit-gstreamer-plugin -y
 dnf install intel-media-driver -y
 dnf copr enable alternateved/eza -y
-dnf copr enable luya/howdy -y
 dnf install audacity eza fastfetch gimp gnome-themes-extra gnome-tweaks google-noto-sans-cjk-fonts google-roboto-fonts handbrake htop inkscape kid3 kleopatra mkvtoolnix-gui nextcloud-client obs-studio python3-dlib python3-tkinter qemu strawberry texstudio timeshift tlp tlp-rdw v4l-utils vim-enhanced virt-manager vlc wireguard-tools xournalpp xsensors yt-dlp -y
-dnf install --setopt=install_weak_deps=false install howdy -y
 flatpak install -y flathub com.github.tchx84.Flatseal com.google.EarthPro org.onlyoffice.desktopeditors us.zoom.Zoom
 systemctl enable --now libvirtd
 usermod -aG libvirt kenneth
@@ -55,11 +53,6 @@ systemctl mask systemd-rfkill.service systemd-rfkill.socket
 sed -i 's/#USB_AUTOSUSPEND=1/USB_AUTOSUSPEND=0/g' /etc/tlp.conf
 systemctl restart tlp
 tlp start
-
-# Configure howdy
-echo "Configuring howdy:"
-sed -i 's/device_path = none/device_path = \/dev\/video2/g' /usr/lib64/security/howdy/config.ini
-sed -i 's/dark_threshold = 50/dark_threshold = 80/g' /usr/lib64/security/howdy/config.ini
 
 # Configure yt-dlp
 echo "Configuring yt-dlp:"
