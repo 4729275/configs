433ebb58a41b8f92e2564f5ccff18e21c12b40a7
4729275 <83101013+4729275@users.noreply.github.com>
2025-05-06 21:27:23 -0400

    Update arch-plasma-setup-root.sh

--- arch-plasma-setup-root.sh
+++ arch-plasma-setup-root.sh
@@ -7,7 +7,7 @@ echo "Arch Linux Setup, Root Portion - Kenneth Simmons, 2025"
 
 # Install packages
 echo "Installing packages:"
-pacman -Sy --noconfirm akregator audacity cdrtools devede dnsmasq dvd+rw-tools eza fastfetch firefox gimp handbrake hplip htop inetutils inkscape jre-openjdk k3b kid3 kleopatra ktorrent lib32-gcc-libs lib32-glibc libaacs libdvdcss libgpod libreoffice-fresh mkvtoolnix-gui musescore nextcloud-client obs-studio psensor qemu-full reflector rhythmbox rocm-opencl-runtime rpi-imager steam texlive-latex texstudio timeshift tk ttf-liberation ttf-roboto udftools ufw v4l2loopback-dkms virt-manager virtualbox vulkan-radeon wireguard-tools yt-dlp
+pacman -Sy --noconfirm akregator audacity cdrdao cdrtools devede dnsmasq dvd+rw-tools eza fastfetch firefox gimp handbrake hplip htop inetutils inkscape jre-openjdk k3b kid3 kleopatra ktorrent lib32-gcc-libs lib32-glibc libaacs libdvdcss libgpod libreoffice-fresh mkvtoolnix-gui musescore nextcloud-client obs-studio psensor qemu-full reflector rhythmbox rocm-opencl-runtime rpi-imager steam texlive-latex texstudio timeshift tk ttf-liberation ttf-roboto udftools ufw v4l2loopback-dkms virt-manager virtualbox vulkan-radeon wireguard-tools yt-dlp
 systemctl enable --now avahi-daemon.service
 sed -i 's/#firewall_backend = "nftables"/firewall_backend = "iptables"/g' /etc/libvirt/network.conf
 systemctl enable --now libvirtd
