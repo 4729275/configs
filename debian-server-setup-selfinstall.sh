8736153dd2ab5e4d0881dc7ff3e0ddd5475f5e51
4729275 <83101013+4729275@users.noreply.github.com>
2025-10-27 07:32:03 -0400

    Update debian-server-setup-selfinstall.sh

--- debian-server-setup-selfinstall.sh
+++ debian-server-setup-selfinstall.sh
@@ -13,9 +13,16 @@ apt-get install -y ca-certificates curl exa ffmpeg gnupg htop snapd systemd-time
 # Install docker
 echo "Installing docker:"
 install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+if [ -f /etc/apt/sources.list.d/docker.sources ]; then
+mv /etc/apt/sources.list.d/docker.sources /etc/apt/sources.list.d/docker.sources.bak
+fi
+echo "Types: deb" >> /etc/apt/sources.list.d/docker.sources
+echo "URIs: https://download.docker.com/linux/debian" >> /etc/apt/sources.list.d/docker.sources
+echo "Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")" >> /etc/apt/sources.list.d/docker.sources
+echo "Components: stable" >> /etc/apt/sources.list.d/docker.sources
+echo "Signed-By: /etc/apt/keyrings/docker.asc" >> /etc/apt/sources.list.d/docker.sources
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 usermod -aG docker kenneth
