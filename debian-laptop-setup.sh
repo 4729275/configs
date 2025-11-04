4fa2baa04ceb53a75ae5bf432b71a5de96c62ae1
4729275 <83101013+4729275@users.noreply.github.com>
2025-11-04 18:05:59 -0500

    Update debian-laptop-setup.sh

--- debian-laptop-setup.sh
+++ debian-laptop-setup.sh
@@ -13,7 +13,7 @@ apt-get full-upgrade -y
 
 # Install packages
 echo "Installing packages:"
-apt-get install audacity eza fastfetch flatpak fonts-noto-cjk fonts-roboto gimp gnome-software-plugin-flatpak gnome-themes-extra handbrake htop inkscape kid3 mkvtoolnix-gui nextcloud-desktop obs-studio plymouth-themes psensor python3-tk rhythmbox texstudio timeshift tlp tlp-rdw ufw v4l2loopback-dkms vim virt-manager vlc wireguard xournalpp yt-dlp -y
+apt-get install audacity eza fastfetch flatpak fonts-noto-cjk fonts-roboto gimp gnome-software-plugin-flatpak gnome-themes-extra handbrake htop inkscape kid3 mkvtoolnix-gui nextcloud-desktop obs-studio plymouth-themes psensor python3-tk rhythmbox systemd-zram-generator texstudio timeshift tlp tlp-rdw ufw v4l2loopback-dkms vim virt-manager vlc wireguard xournalpp yt-dlp -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.github.tchx84.Flatseal org.onlyoffice.desktopeditors -y
 git clone https://gitlab.gnome.org/GNOME/adwaita-fonts /home/kenneth/adwaita-fonts
@@ -30,6 +30,15 @@ echo "Configuring firewall:"
 ufw default allow outgoing
 ufw default deny incoming
 ufw enable
+
+# Configure swap-on-zram:
+echo "Configuring swap-on-zram:"
+if [ -f /etc/systemd/zram-generator.conf ]; then
+mv /etc/systemd/zram-generator.conf /etc/systemd/zram-generator.conf.bak
+fi
+echo "[zram0]" >> /etc/systemd/zram-generator.conf
+echo "zram-size = min(ram, 8192)" >> /etc/systemd/zram-generator.conf
+echo "compression-algorithm = zstd" >> /etc/systemd/zram-generator.conf
 
 # Configure tlp
 echo "Configuring tlp:"
