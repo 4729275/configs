4e9cf57ade348b99d12f8bb96b631cf08157ce44
4729275 <83101013+4729275@users.noreply.github.com>
2025-11-23 09:50:44 -0500

    Update debian-laptop-setup.sh

--- debian-laptop-setup.sh
+++ debian-laptop-setup.sh
@@ -13,9 +13,9 @@ apt-get full-upgrade -y
 
 # Install packages
 echo "Installing packages:"
-apt-get install audacity eza fastfetch flatpak fonts-noto-cjk fonts-roboto gimp gnome-software-plugin-flatpak gnome-themes-extra handbrake htop inkscape kid3 mkvtoolnix-gui nextcloud-desktop obs-studio plymouth-themes psensor python3-tk rhythmbox systemd-zram-generator texstudio timeshift tlp tlp-rdw ufw v4l2loopback-dkms vim virt-manager vlc wireguard xournalpp yt-dlp -y
+apt-get install audacity eza fastfetch flatpak fonts-noto-cjk fonts-roboto gnome-software-plugin-flatpak gnome-themes-extra htop nextcloud-desktop plymouth-themes psensor python3-tk rhythmbox systemd-zram-generator texstudio timeshift tlp tlp-rdw ufw v4l2loopback-dkms vim virt-manager wireguard yt-dlp -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.github.tchx84.Flatseal com.google.EarthPro org.onlyoffice.desktopeditors -y
+flatpak install flathub com.github.tchx84.Flatseal com.google.EarthPro org.gimp.GIMP fr.handbrake.ghb org.inkscape.Inkscape org.kde.kid3 org.bunkus.mkvtoolnix-gui com.obsproject.Studio org.onlyoffice.desktopeditors org.videolan.VLC com.github.xournalpp.xournalpp -y
 flatpak override --env=QT_DEVICE_PIXEL_RATIO=2 com.google.EarthPro
 git clone https://gitlab.gnome.org/GNOME/adwaita-fonts /home/kenneth/adwaita-fonts
 mkdir /home/kenneth/.local/share/fonts
@@ -80,7 +80,7 @@ fi
 
 # Remove packages
 echo "Removing packages:"
-apt-get purge evince gnome-terminal -y
+apt-get purge evince evolution gnome-maps gnome-music gnome-terminal shotwell -y
 apt-get autoremove -y
 
 echo "Complete!"
