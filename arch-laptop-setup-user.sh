dc9fc50b873e36bfc48f8011dd59d2ec40c8329f
4729275 <83101013+4729275@users.noreply.github.com>
2025-11-25 10:23:40 -0500

    Update arch-laptop-setup-user.sh

--- arch-laptop-setup-user.sh
+++ arch-laptop-setup-user.sh
@@ -19,9 +19,9 @@ echo "Installing AUR packages - manual confirmation required:"
 paru -S mullvad-vpn-bin zoom
 
 # Install Flatpak packages
-echo "Installing Flatpak packages:"
+echo "Installing Flatpak packages - manual confirmation required:"
 flatpak install -y flathub com.github.tchx84.Flatseal com.google.EarthPro io.gitlab.news_flash.NewsFlash org.onlyoffice.desktopeditors
-flatpak override --env=QT_DEVICE_PIXEL_RATIO=2 com.google.EarthPro
+sudo flatpak override --env=QT_DEVICE_PIXEL_RATIO=2 com.google.EarthPro
 
 # Configure bash prompt
 echo "Configuring bash prompt:"
