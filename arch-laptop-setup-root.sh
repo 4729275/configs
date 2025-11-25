d3fffa86700c29d48119a9d9816258fd900259e5
4729275 <83101013+4729275@users.noreply.github.com>
2025-11-25 10:43:42 -0500

    Update arch-laptop-setup-root.sh

--- arch-laptop-setup-root.sh
+++ arch-laptop-setup-root.sh
@@ -7,7 +7,7 @@ echo "Arch Linux Laptop Setup, Root Portion - Kenneth Simmons, 2025"
 
 # Install packages
 echo "Installing packages:"
-pacman -Sy --noconfirm audacity eza fastfetch firefox gimp gvfs-dnssd handbrake hplip htop inkscape kid3 libreoffice-still mkvtoolnix-gui nextcloud-client obs-studio plymouth psensor qemu-full qt6-wayland reflector rhythmbox texlive-latex texstudio timeshift tk tlp tlp-rdw ttf-liberation ttf-roboto ufw v4l2loopback-dkms virt-manager vlc wireguard-tools xournalpp yt-dlp
+pacman -Sy --noconfirm audacity eza fastfetch firefox fwupd gimp gvfs-dnssd handbrake hplip htop inkscape kid3 libreoffice-still mkvtoolnix-gui nextcloud-client obs-studio plymouth psensor qemu-full qt6-wayland reflector rhythmbox texlive-latex texstudio timeshift tk tlp tlp-rdw ttf-liberation ttf-roboto ufw v4l2loopback-dkms virt-manager vlc wireguard-tools xournalpp yt-dlp
 sed -i 's/#firewall_backend = "nftables"/firewall_backend = "iptables"/g' /etc/libvirt/network.conf
 systemctl enable --now avahi-daemon.service libvirtd tlp ufw
 usermod -aG libvirt kenneth
@@ -46,6 +46,16 @@ systemctl mask systemd-rfkill.service systemd-rfkill.socket
 sed -i 's/#USB_AUTOSUSPEND=1/USB_AUTOSUSPEND=0/g' /etc/tlp.conf
 systemctl restart tlp
 tlp start
+
+# Configure fwupd
+echo "Configuring fwupd:"
+sbctl sign -s -o /usr/lib/fwupd/efi/fwupdx64.efi.signed /usr/lib/fwupd/efi/fwupdx64.efi
+if [ ! -f /etc/fwupd/fwupd.conf.bak ]; then
+cp /etc/fwupd/fwupd.conf /etc/fwupd/fwupd.conf.bak
+echo "[uefi_capsule]" >> /etc/fwupd/fwupd.conf
+echo "DisableShimForSecureBoot=true" >> /etc/fwupd/fwupd.conf
+systemctl restart fwupd
+fi
 
 # Configure yt-dlp
 echo "Configuring yt-dlp:"
