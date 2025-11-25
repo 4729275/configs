2b2089b2e9805608afad4dc9cd00f6a532cbab17
4729275 <83101013+4729275@users.noreply.github.com>
2025-11-24 23:32:06 -0500

    Create arch-laptop-setup-user.sh

---
+++ arch-laptop-setup-user.sh
@@ -0,0 +1,46 @@
+#! /usr/bin/bash
+
+### Arch Linux Laptop Setup Script, User Portion ###
+# Kenneth Simmons, 2025
+
+echo "Arch Linux Laptop Setup, User Portion - Kenneth Simmons, 2025"
+
+# Install AUR helper
+echo "Installing AUR helper - manual confirmation required:"
+cd /home/kenneth
+git clone https://aur.archlinux.org/paru-bin
+cd paru-bin
+makepkg -si
+cd ..
+rm -rf paru-bin
+
+# Install AUR packages
+echo "Installing AUR packages - manual confirmation required:"
+paru -S mullvad-vpn-bin zoom
+
+# Install Flatpak packages
+echo "Installing Flatpak packages:"
+flatpak install -y flathub com.github.tchx84.Flatseal com.google.EarthPro io.gitlab.news_flash.NewsFlash org.onlyoffice.desktopeditors
+
+# Configure bash prompt
+echo "Configuring bash prompt:"
+if [ -f /home/kenneth/.bashrc ]; then
+mv /home/kenneth/.bashrc /home/kenneth/.bashrc.bak
+fi
+echo "PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> /home/kenneth/.bashrc
+
+# Create bash aliases
+echo "Creating bash aliases:"
+echo "if [ -f ~/.bash_aliases ]; then" >> /home/kenneth/.bashrc
+echo ". ~/.bash_aliases" >> /home/kenneth/.bashrc
+echo "fi" >> /home/kenneth/.bashrc
+if [ -f /home/kenneth/.bash_aliases ]; then
+mv /home/kenneth/.bash_aliases /home/kenneth/.bash_aliases.bak
+fi
+echo "alias pacup='paru && flatpak update'" >> /home/kenneth/.bash_aliases
+echo "alias ls='eza -al --group-directories-first'" >> /home/kenneth/.bash_aliases
+echo "alias wgup='sudo wg-quick up home'" >> /home/kenneth/.bash_aliases
+echo "alias wgdn='sudo wg-quick down home'" >> /home/kenneth/.bash_aliases
+
+echo "User portion complete!"
+echo "Reboot the system to finalize the changes."
