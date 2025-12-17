26335542629561038a49ccbf1f94108e5e92bae0
4729275 <83101013+4729275@users.noreply.github.com>
2025-12-17 03:09:49 -0500

    Update arch-setup-root.sh

--- arch-setup-root.sh
+++ arch-setup-root.sh
@@ -60,7 +60,7 @@ mv /etc/yt-dlp.conf /etc/yt-dlp.conf.bak
 fi
 echo "-P /home/kenneth/Downloads/" >> /etc/yt-dlp.conf
 echo "-x" >> /etc/yt-dlp.conf
-echo "--audio-format best" >> /etc/yt-dlp.conf
+echo "--audio-format opus" >> /etc/yt-dlp.conf
 echo "-o \"%(title)s.%(ext)s\"" >> /etc/yt-dlp.conf
 
 echo "Root portion complete!"
