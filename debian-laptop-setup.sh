7881706599b486033ccaaa0a3ba35fe912a3c258
4729275 <83101013+4729275@users.noreply.github.com>
2025-12-18 03:53:25 -0500

    Update debian-laptop-setup.sh

--- debian-laptop-setup.sh
+++ debian-laptop-setup.sh
@@ -67,6 +67,8 @@ echo "-P /home/kenneth/Downloads/" >> /etc/yt-dlp.conf
 echo "-x" >> /etc/yt-dlp.conf
 echo "--audio-format opus" >> /etc/yt-dlp.conf
 echo "-o \"%(title)s.%(ext)s\"" >> /etc/yt-dlp.conf
+echo "--cookies-from-browser firefox" >> /etc/yt-dlp.conf
+echo "--remote-components ejs:github" >> /etc/yt-dlp.conf
 
 # Create bash aliases
 echo "Creating bash aliases:"
