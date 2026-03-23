6f7cbc3603d37a244a1cddef182db61a7ecc6d78
4729275 <83101013+4729275@users.noreply.github.com>
2026-03-23 17:54:44 -0400

    Added nextcloud-client as pacman package

--- arch-setup-root.sh
+++ arch-setup-root.sh
@@ -7,7 +7,7 @@ echo "Arch Linux Setup, Root Portion - Kenneth Simmons, 2026"
 
 # Install packages
 echo "Installing packages:"
-pacman -Sy --noconfirm audacity cdrdao cdrtools devede dnsmasq dvd+rw-tools eza fastfetch firefox gimp handbrake hplip htop inetutils inkscape jre-openjdk k3b kid3 libaacs libdvdcss libgpod libreoffice-fresh mkvtoolnix-gui musescore obs-studio plymouth qemu-full qt6-wayland reflector rhythmbox rpi-imager snapper steam texlive-latex texstudio timeshift tk transmission-gtk ttf-liberation ttf-roboto udftools ufw v4l2loopback-dkms virt-manager virtualbox virtualbox-guest-iso virtualbox-host-modules-arch vlc vlc-plugin-ffmpeg vulkan-radeon wireguard-tools yt-dlp yt-dlp-ejs
+pacman -Sy --noconfirm audacity cdrdao cdrtools devede dnsmasq dvd+rw-tools eza fastfetch firefox gimp handbrake hplip htop inetutils inkscape jre-openjdk k3b kid3 libaacs libdvdcss libgpod libreoffice-fresh mkvtoolnix-gui musescore nextcloud-client obs-studio plymouth qemu-full qt6-wayland reflector rhythmbox rpi-imager snapper steam texlive-latex texstudio timeshift tk transmission-gtk ttf-liberation ttf-roboto udftools ufw v4l2loopback-dkms virt-manager virtualbox virtualbox-guest-iso virtualbox-host-modules-arch vlc vlc-plugin-ffmpeg vulkan-radeon wireguard-tools yt-dlp yt-dlp-ejs
 sed -i 's/#firewall_backend = "nftables"/firewall_backend = "iptables"/g' /etc/libvirt/network.conf
 systemctl enable --now avahi-daemon.service libvirtd udisks2 ufw
 usermod -aG libvirt kenneth
